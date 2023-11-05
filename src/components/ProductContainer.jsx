07656a663fc4a6e52d88419c8240f2c701308841
pr0fix <bgx152@myy.haaga-helia.fi>
2023-11-05 10:16:30 +0200

    Added AgGrid

--- src/components/ProductContainer.jsx
+++ src/components/ProductContainer.jsx
@@ -1,10 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import axios from 'axios';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-material.css';
 
 export default function Productcontainer() {
 
     const [productList, setProductList] = useState([]);
-    const REST_URL = 'http://localhost:8080/api/products'
+    const REST_URL = 'http://localhost:8080'
     const [loading, setLoading] = useState(true);
 
 
@@ -13,10 +16,9 @@ export default function Productcontainer() {
     }, []);
 
     const listProducts = () => {
-        axios.get(`${REST_URL}`)
+        axios.get(`${REST_URL}/api/products`)
             .then((response) => {
                 setProductList(response.data._embedded.products);
-                console.log(response.data._embedded.products);
                 setLoading(false);
             })
             .catch((error) => {
@@ -25,22 +27,40 @@ export default function Productcontainer() {
             });
     }
 
-    const renderProducts = productList.map((product, index) => (
-        <li key={index}>
-            {product.name}
-            <p>
-                {product.description}</p>
-        </li>
-    ));
+    const columns = [
+        { headerName: 'Product name', field: 'name' },
+        { headerName: 'Price', field: 'price' },
+        { headerName: 'Description', field: 'description' },
+        { headerName: 'Color', field: 'color' },
+        { headerName: 'Year of manufacturing', field: 'manifacturingYear' }
+    ];
 
+    const defaultColDef = useMemo(() => ({
+        resizable: true,
+        flex: 1,
+        minWidth: 250
+    }));
 
+    const gridOptions = {
+        domLayout: 'autoHeight'
+    };
 
     return (
-        <div>
-            <h1>Product list</h1>
+        <>
+        <h1>Bikestore</h1>
+        <div className='ag-theme-material' style={{height: '100%', width: '1500px' }}>
+            <AgGridReact
+                defaultColDef={defaultColDef}
+                rowData={productList}
+                columnDefs={columns}
+                gridOptions={gridOptions}
+                pagination={true}
+                paginationPageSize={5}
+            >
 
-            {loading ? <p>Loading...</p> : <ul> {renderProducts}</ul>}
+            </AgGridReact>
 
         </div>
+        </>
     )
 }
